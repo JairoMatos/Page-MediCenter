bf8c22404a64fffc43d084794d2d414d3e216d88
JairoMatos <31500074+JairoMatos@users.noreply.github.com>
2022-03-19 18:18:44 -0300

    commit inicial

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+
+/*
+Tabuada simplificada 1 ao 10
+....
+....
+*/
+for(let x=1; x<=20; x++){
+    for(let i=1; i<=20; i++){
+        console.log(`${x} x ${i} = ${x*i}`)
+    }
+    console.log(`-------------TABUADA:`)
+}
